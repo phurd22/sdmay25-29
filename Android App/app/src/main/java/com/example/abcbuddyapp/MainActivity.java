5ee13b4bbc6b13097d87f26120aad5b3fc1f0f44
Zachary Scurlock <scurlock@iastate.edu>
2024-11-14 12:45:57 -0600

    made a very rough prototype for the UI

--- Android App/app/src/main/java/com/example/abcbuddyapp/MainActivity.java
+++ Android App/app/src/main/java/com/example/abcbuddyapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.abcbuddyapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button0, button1, button2, button3, button4, button5, button6, button7, button8, button9, buttonClear, buttonVariable;
+    TextView equation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        equation = findViewById(R.id.equation);
+        button0.findViewById(R.id.button0);
+        button1.findViewById(R.id.button1);
+        button2.findViewById(R.id.button2);
+        button3.findViewById(R.id.button3);
+        button4.findViewById(R.id.button4);
+        button5.findViewById(R.id.button5);
+        button6.findViewById(R.id.button6);
+        button7.findViewById(R.id.button7);
+        button8.findViewById(R.id.button8);
+        button9.findViewById(R.id.button9);
+        buttonClear.findViewById(R.id.buttonClear);
+        buttonVariable.findViewById(R.id.buttonVariable);
     }
 }
