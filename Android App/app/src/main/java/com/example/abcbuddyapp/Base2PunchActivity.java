4580db718f71da8a9ba0b833c1beca48d4eb8969
Connor <connor66@iastate.edu>
2025-02-13 16:30:14 -0600

    Progress on base2 punch design

--- Android App/app/src/main/java/com/example/abcbuddyapp/Base2PunchActivity.java
+++ Android App/app/src/main/java/com/example/abcbuddyapp/Base2PunchActivity.java
@@ -7,12 +7,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Base2PunchActivity extends AppCompatActivity {
 
     private Base2PunchView base2PunchView;
+    private int currentPage;
+    private int totalPages;
+    private List<List<Long>> allNumbers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +29,29 @@ public class Base2PunchActivity extends AppCompatActivity {
             return insets;
         });
 
+        currentPage = 1;
+        totalPages = 3;
+
         base2PunchView = findViewById(R.id.base2PunchView);
 
-        List<Long> numbers = Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L,
+        List<Long> numbers1 = Arrays.asList(1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L, 10L,
                 -1L, -2L, -3L, -4L, -5L, -6L, -7L, -8L, -9L, -10L,
                 100L, 200L, 300L, 400L, 500L, 600L, 700L, 800L, 900L, 1000L);
-        base2PunchView.setNumbers(numbers);
+        List<Long> numbers2 = Arrays.asList();
+        List<Long> numbers3 = Arrays.asList();
+
+        allNumbers.add(numbers1);
+        allNumbers.add(numbers2);
+        allNumbers.add(numbers3);
+
+        base2PunchView.setDrawLeftArrow(true);
+        base2PunchView.setDrawRightArrow(true);
+        if (currentPage == 1) {
+            base2PunchView.setDrawLeftArrow(false);
+        }
+        if (currentPage == totalPages) {
+            base2PunchView.setDrawRightArrow(false);
+        }
+        base2PunchView.setNumbers(allNumbers.get(currentPage - 1), currentPage, totalPages);;
     }
 }
--- Android App/app/src/main/java/com/example/abcbuddyapp/Base2PunchView.java
+++ Android App/app/src/main/java/com/example/abcbuddyapp/Base2PunchView.java
@@ -14,11 +14,20 @@ import java.util.List;
 
 public class Base2PunchView extends View {
 
+    //TODO: Need to add ability to switch pages. Add touch event listener for
+    // the left and right arrow buttons.
+
     private final int dotRadius = 8; // Dot size for grid points
     private int xOffset;
-    private int yOffset;
+    private int botYOffset;
+    private int topYOffset;
     private int xSpacer;
     private int ySpacer;
+    private boolean drawLeftArrow;
+    private boolean drawRightArrow;
+
+    private int currentPage = 1;
+    private int totalPages = 1;
 
     private List<String> bitArray = new ArrayList<>(); // Holds 50-bit binary strings
     private Paint dotPaint;
@@ -56,17 +65,20 @@ public class Base2PunchView extends View {
         int screenWidth = displayMetrics.widthPixels;
         int screenHeight = displayMetrics.heightPixels;
 
-        xOffset = screenWidth / 40;
-        yOffset = screenHeight / 40;
+        xOffset = screenWidth / 10;
+        botYOffset = screenHeight / 20;
+        topYOffset = screenHeight / 33;
         xSpacer = (screenWidth - 2 * xOffset) / 29;
-        ySpacer = (screenHeight - 2 * yOffset) / 49;
+        ySpacer = (screenHeight - (botYOffset + topYOffset)) / 49;
     }
 
-    public void setNumbers(List<Long> numbers) {
+    public void setNumbers(List<Long> numbers, int currentPage, int totalPages) {
         bitArray.clear();
         for (long number : numbers) {
             bitArray.add(toTwosComplement50Bit(number));
         }
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
         invalidate(); // Redraw the view with new data
     }
 
@@ -88,21 +100,51 @@ public class Base2PunchView extends View {
             for (int col = 0; col < bits.length(); col++) {
                 if (bits.charAt(col) == '1') {
                     float x = xOffset + row * xSpacer;
-                    float y = yOffset + col * ySpacer;
+                    float y = topYOffset + col * ySpacer;
                     canvas.drawCircle(x, y, dotRadius, dotPaint);
                 }
             }
         }
 
         drawLabels(canvas);
+        drawNavigationIndicators(canvas);
     }
 
     private void drawLabels(Canvas canvas) {
         Paint textPaint = new Paint();
         textPaint.setColor(Color.BLACK);
-        textPaint.setTextSize(30);
+        textPaint.setTextSize(45);
 
-        canvas.drawText("b0", xSpacer, yOffset + 50 * ySpacer + 40, textPaint);
-        canvas.drawText("n0", xOffset, yOffset + 50 * ySpacer + 80, textPaint);
+        canvas.drawText("b0", xOffset / 2, (topYOffset + ySpacer * 50) - 25, textPaint);
+        canvas.drawText("b49", xOffset / 2 - 20, topYOffset + 10, textPaint);
+        canvas.drawText("n0", xOffset - 25, topYOffset + 50 * ySpacer + 17, textPaint);
+    }
+
+    private void drawNavigationIndicators(Canvas canvas) {
+        Paint textPaint = new Paint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setTextSize(60);
+
+        // Draw left arrow
+        if (drawLeftArrow) {
+            canvas.drawText("<", xOffset / 4, getHeight() / 2, textPaint);
+        }
+
+        // Draw right arrow
+        if (drawRightArrow) {
+            canvas.drawText(">", getWidth() - xOffset / 2, getHeight() / 2, textPaint);
+        }
+
+        // Draw page count in bottom right
+        String pageText = currentPage + " / " + totalPages;
+        canvas.drawText(pageText, getWidth() - xOffset, getHeight() - botYOffset / 2 + 15, textPaint);
+    }
+
+    public void setDrawLeftArrow(boolean drawLeftArrow) {
+        this.drawLeftArrow = drawLeftArrow;
+    }
+
+    public void setDrawRightArrow(boolean drawRightArrow) {
+        this.drawRightArrow = drawRightArrow;
     }
 }
